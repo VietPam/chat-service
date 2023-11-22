bd6e518e6e5443d01a7cd53122a10d87c202abf7
vietpham-stvg <82923647+phamquangviet891@users.noreply.github.com>
2023-11-22 23:14:14 +0700

    setup
    setup project with db

---
+++ Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace chat_service_se357.Models
+{
+    public class DataContext: DbContext
+    {
+        public static string configSql = "Host=ep-silent-hill-65750190.ap-southeast-1.postgres.vercel-storage.com:5432;Database=verceldb;Username=default;Password=8isowjIMlJG4";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(configSql);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,35 +1,87 @@
+using chat_service_se357.Models;
+using Microsoft.EntityFrameworkCore;
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
+
 namespace chat_service_se357
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Debug()
+                   .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                   .WriteTo.File("mylog.txt", rollingInterval: RollingInterval.Day)
+                   .CreateLogger();
+            try
             {
+
+                var builder = WebApplication.CreateBuilder(args);
+                builder.WebHost.ConfigureKestrel((context, option) =>
+                {
+                    option.ListenAnyIP(50000, listenOptions =>
+                    {
+                        // listenOptions.UseHttps("./smartlook.com.vn.pfx", "stvg");
+
+                    });
+                    option.Limits.MaxConcurrentConnections = null;
+                    option.Limits.MaxRequestBodySize = null;
+                    option.Limits.MaxRequestBufferSize = null;
+                });
+                // Add services to the container.
+                //builder.Logging.AddSerilog();
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("HTTPSystem", builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).WithExposedHeaders("Grpc-Status", "Grpc-Encoding", "Grpc-Accept-Encoding");
+                    });
+                });
+
+                builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(DataContext.configSql));
+                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                using (var scope = app.Services.CreateScope())
+                {
+                    IServiceProvider services = scope.ServiceProvider;
+                    DataContext datacontext = services.GetRequiredService<DataContext>();
+                    datacontext.Database.EnsureCreated();
+                    await datacontext.Database.MigrateAsync();
+
+                }
+
+                Log.Information(String.Format("Connected to Server {0} : {1} ", DateTime.Now, DataContext.configSql));
+
+                // Configure the HTTP request pipeline.
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+
+                app.UseCors("HTTPSystem");
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+                app.MapGet("/", () => string.Format("Server Chat of SE347- {0}", DateTime.Now));
+
+                app.Run();
+
+                Log.CloseAndFlush();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+            }
         }
     }
 }
